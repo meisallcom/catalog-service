358d5bd3aba0fcdac90ee2f616d722eabdae50d0
Perry Zhang <perry@meisall.com>
2023-07-06 13:23:05 +0800

    initial for demo

---
+++ src/main/java/com/gv/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.gv.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
