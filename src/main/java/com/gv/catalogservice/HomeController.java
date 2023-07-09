39c2132c591fabe358ab24660b3e4a2b7319e77d
Perry Zhang <perry@meisall.com>
2023-07-09 17:51:09 +0800

    new properties  for configuration

--- src/main/java/com/gv/catalogservice/HomeController.java
+++ src/main/java/com/gv/catalogservice/HomeController.java
@@ -1,12 +1,20 @@
 package com.gv.catalogservice;
 
+import com.gv.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the test env11!";
+        return polarProperties.getGreeting();
     }
 }
