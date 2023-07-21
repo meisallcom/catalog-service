9c21eba7afae693855c3211e39934c40e5112ba6
Perry Zhang <perry@meisall.com>
2023-07-21 16:04:48 +0800

    flyway demo

--- src/main/java/com/gv/catalogservice/domain/Book.java
+++ src/main/java/com/gv/catalogservice/domain/Book.java
@@ -31,6 +31,8 @@ public record Book (
     @Positive(message = "The book price must be greater than zero.")
     Double price,
 
+    String publisher,
+
     @CreatedDate
     Instant createdDate,
 
@@ -42,10 +44,10 @@ public record Book (
 
 ){
     public static Book of(
-        String isbn, String title, String author, Double price
+        String isbn, String title, String author, Double price, String publisher
     ){
         return new Book(
-        null, isbn, title, author, price,  null, null,0
+        null, isbn, title, author, price,  publisher,null, null,0
         );
     }
 }
