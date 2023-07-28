dc09326e8878446e509e12973e736943b99afae9
Perry Zhang <perry@meisall.com>
2023-07-28 18:12:27 +0800

    test fix --

--- src/test/java/com/gv/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/gv/catalogservice/web/BookJsonTests.java
@@ -48,6 +48,6 @@ public class BookJsonTests {
                 """;
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(new Book(394L, "1234567890", "Title", "Author", 9.90, instant, instant, 21));
+                .isEqualTo(new Book(394L, "1234567890", "Title", "Author", 9.90, "Polarsophia",instant, instant, 21));
     }
 }
