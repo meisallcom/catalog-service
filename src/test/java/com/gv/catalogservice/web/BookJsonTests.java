f1670b5f1517066c1444eae08edc56cb5f424093
Perry Zhang <perry@meisall.com>
2023-07-28 18:10:57 +0800

    test fix

--- src/test/java/com/gv/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/gv/catalogservice/web/BookJsonTests.java
@@ -19,7 +19,7 @@ public class BookJsonTests {
     @Test
     void testSerialize() throws Exception {
         var now = Instant.now();
-        var book = new Book(394L,"1234567890", "Title", "Author", 9.90, now, now,21);
+        var book = new Book(394L,"1234567890", "Title", "Author", 9.90,"Polarsophia", now, now,21);
         var jsonContent = json.write(book);
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
                 .isEqualTo(book.isbn());
